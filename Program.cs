be9a2bf59e1bd17e03c96d738686fa2955d29417
AdeyBoy123 <groveraden@gmail.com>
2023-09-15 15:54:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeAbba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string: ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Input another string: ");
+            string b = Console.ReadLine();
+
+            Console.WriteLine(makeAbba(a, b));
+            Console.ReadLine();
+        }
+
+        static string makeAbba(string a, string b)
+        {
+            return a + b + b + a;
+            
+        }
+    }
+}
